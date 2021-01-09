f559be32d0328d3b542a5170137ee15d23d9a292
Jiri Bruchanov <jbruchanov@gmail.com>
2021-01-09 18:50:04 +0000

    Update PlatformViewModel to have onCleared

--- core/src/androidMain/kotlin/com/scurab/dipho/common/arch/PlatformViewModel.kt
+++ core/src/androidMain/kotlin/com/scurab/dipho/common/arch/PlatformViewModel.kt
@@ -2,4 +2,9 @@ package com.scurab.dipho.common.arch
 
 import androidx.lifecycle.ViewModel
 
-actual open class PlatformViewModel : ViewModel()
+actual open class PlatformViewModel : ViewModel() {
+
+    actual override fun onCleared() {
+        super.onCleared()
+    }
+}
--- core/src/commonMain/kotlin/com/scurab/dipho/common/arch/PlatformViewModel.kt
+++ core/src/commonMain/kotlin/com/scurab/dipho/common/arch/PlatformViewModel.kt
@@ -1,3 +1,5 @@
 package com.scurab.dipho.common.arch
 
-expect open class PlatformViewModel()
+expect open class PlatformViewModel() {
+    protected open fun onCleared()
+}
